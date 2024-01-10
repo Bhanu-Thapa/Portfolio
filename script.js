f2c1638feb51c2c3d00cf7aea3b81e3f665ded4f
BhanuThapa <bhanuthapa252@gmail.com>
2024-01-11 02:55:34 +0530

    first code of my portfolio - parallax effect

---
+++ script.js
@@ -0,0 +1,14 @@
+let leaf = document.querySelector('.leaf');
+let hill1 = document.querySelector('.hill1');
+let hill4 = document.querySelector('.hill4');
+let hill5 = document.querySelector('.hill5');
+
+window.addEventListener('scroll', () => {
+  let value = window.scrollY;
+  console.log(value);
+  leaf.style.left = `${value * 1.5}px`;
+  leaf.style.top = `-${value * 1.5}px`;
+  hill1.style.top = `${value * 1.5}px`;
+  hill4.style.left = `${-value * 1.5}px`;
+  hill5.style.left = `${value * 1.5}px`;
+});
