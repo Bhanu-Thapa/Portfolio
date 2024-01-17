d77ed5f18dba28cb7a91b15e80bdaaeeca5b96d7
BhanuThapa <bhanuthapa252@gmail.com>
2024-01-18 03:49:14 +0530

    styling slider project section done

--- script.js
+++ script.js
@@ -12,3 +12,35 @@ window.addEventListener('scroll', () => {
   hill4.style.left = `${-value * 1.5}px`;
   hill5.style.left = `${value * 1.5}px`;
 });
+
+// project slider
+
+let leftArrow = document.querySelector('.arrow-left');
+let rightArrow = document.querySelector('.arrow-right');
+
+let projectImg = document.querySelector('.project-img');
+let projectList = document.querySelector('.project-list');
+let projectCart = document.querySelectorAll('.project-cart');
+
+let projectThumnail = document.querySelector('.project-thumnail');
+let proThumCard = projectThumnail.querySelectorAll('.pro-thum-card');
+
+count = 0;
+rightArrow.addEventListener('click', () => {
+  if (count < projectCart.length - 1) {
+    console.log(proThumCard);
+    console.log(proThumCard[count]);
+    projectThumnail.appendChild(proThumCard[count]);
+    count++;
+    console.log(projectCart[count]);
+    projectList.appendChild(projectCart[count]);
+    projectCart[count].style.zIndex = '1';
+    projectImg.classList.add('next');
+  } else {
+    projectThumnail.appendChild(proThumCard[count]);
+    count = 0;
+    projectList.appendChild(projectCart[count]);
+    projectCart[count].style.zIndex = '1';
+    projectImg.classList.add('next');
+  }
+});
